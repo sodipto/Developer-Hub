b95ce14b4222216c60ef7d734b72afd89f14b771
Sodipto Saha <badhansaha444@gmail.com>
2018-09-11 20:41:55 +0600

    Developer Model Add

---
+++ Developer.ProjectChat.Entites/Developer.Entites/Developer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Developer.ProjectChat.Entites.Developer.Entites
+{
+    public class Developer
+    {
+        [Key]
+        public int DeveloperID { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+      
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Confirm password and password do not match")]
+        [NotMapped]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; }
+
+        
+
+
+
+
+    }
+}
