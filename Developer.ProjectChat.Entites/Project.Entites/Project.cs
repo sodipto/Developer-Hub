da9af19cf8d08cd7c8cbd1b60c33fdaba7d08644
Sodipto Saha <badhansaha444@gmail.com>
2018-09-11 20:56:42 +0600

    Project Entites Add

---
+++ Developer.ProjectChat.Entites/Project.Entites/Project.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Developer.ProjectChat.Entites.Project.Entites
+{
+    public class Project
+    {
+        [Key]
+        public int ProjectId { get; set; }
+
+
+        public string ProjectName { get; set; }
+
+
+        public string ProgrammingLanguage { get; set; }
+
+
+        public string  Status { get; set; }
+
+
+
+    }
+}
