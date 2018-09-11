cafe6288798830e2f82d6d56d243e39421da5313
Sodipto Saha <badhansaha444@gmail.com>
2018-09-11 22:45:10 +0600

    ProjectDeveloper.cs Model Add

--- Developer.ProjectChat.Entites/Post.Entites/ProjectChat.cs
+++ Developer.ProjectChat.Entites/Post.Entites/ProjectChat.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +9,18 @@ namespace Developer.ProjectChat.Entites.Post.Entites
 {
     public class ProjectChat
     {
+        [Key]
         public int ProjectChatId { get; set; }
 
         //foreignkey
+        [Required]
         public int ProjectId { get; set; }
 
         //foreignkey
+        [Required]
         public int DeveloperId { get; set; }
 
+        [Required]
         public string Comment { get; set; }
 
         public string FileUrl { get; set; }
---
+++ Developer.ProjectChat.Entites/Project.Entites/ProjectDeveloper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Developer.ProjectChat.Entites.Project.Entites
+{
+    public class ProjectDeveloper
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int DeveloperId { get; set; }
+
+
+    }
+}
