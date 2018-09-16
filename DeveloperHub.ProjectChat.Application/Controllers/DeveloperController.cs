0bed61994eada76a40f44200fd82da4286f3f6d8
Sodipto Saha <badhansaha444@gmail.com>
2018-09-16 20:34:30 +0600

    Developer Controller Add And Parital Page Create

---
+++ DeveloperHub.ProjectChat.Application/Controllers/DeveloperController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeveloperHub.ProjectChat.Application.Controllers
+{
+    public class DeveloperController : Controller
+    {
+        // GET: Developer
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
